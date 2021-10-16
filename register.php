daa7d6cf8ceee057e6cea2107508057a3ccc59d4
unknown <vishal.chennuri@gmail.com>
2021-10-16 23:46:12 +0530

    Changes in Login Credentials and Navbar

--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <?php
 // Include config file
-require_once "config.php";
+require "config.php";
  
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$username = $password = $confirm_password = $address = $email = $mobileno = "";
+$username_err = $password_err = $confirm_password_err = $email_err = $address_err = $mobileno_err = $pincode = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -44,8 +44,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate password
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
+    } elseif(strlen(trim($_POST["password"])) < 8){
+        $password_err = "Passwords must have atleast 8 characters.";
     } else{
         $password = trim($_POST["password"]);
     }
@@ -59,22 +59,63 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $confirm_password_err = "Password did not match.";
         }
     }
+
+    $mobileno = $_POST["mobileno"];  
+    if (!preg_match ("/^[0-9]*$/", $mobileno)){  
+        $mobileno_err = "Only numeric value is allowed.";
+    } elseif(strlen(trim($_POST["mobileno"])) < 10){
+        $mobileno_err = "Please Enter a Valid Number.";
+    }
+
+
+    $email = $_POST ["email"];  
+    $pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";  
+    if (!preg_match ($pattern, $email) ){  
+        $email_err = "Email is not valid.";
+    }
+
+
+    if(empty(trim($_POST["address"]))){
+        $address_err = "Please Enter your Address.";     
+    } else{
+        $address = trim($_POST["address"]);
+    }
+
+
+
+    $pincode = $_POST["pincode"];  
+    if (!preg_match ("/^[0-9]*$/", $pincode) ){  
+        $pincode_err = "Only numeric value is allowed.";   
+    } elseif(empty(trim($_POST["pincode"]))){
+        $pincode_err = "Please enter Pincode.";     
+    } elseif(strlen(trim($_POST["pincode"])) < 6){
+        $pincode_err = "Pincode must have 6 Digits.";
+    } else{
+        $pincode = trim($_POST["pincode"]);
+    }
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err) && empty($mobileno_err) && empty($address_err) && empty($pincode_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $sql = "INSERT INTO users (username, password, email, mobileno, pincode, address) VALUES (:username, :password, :email, :mobileno, :pincode, :address)";
          
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
             $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
+            $stmt->bindParam(":mobileno", $param_mobileno, PDO::PARAM_STR);
+            $stmt->bindParam(":address", $param_address, PDO::PARAM_STR);
+            $stmt->bindParam(":pincode", $param_pincode, PDO::PARAM_STR);
             
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
+            $param_email = $email;
+            $param_mobileno = $mobileno;
+            $param_address = $address;
+            $param_pincode = $pincode;
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Redirect to login page
@@ -91,6 +132,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Close connection
     unset($pdo);
 }
+
+
+
 ?>
  
 <!DOCTYPE html>
@@ -126,23 +170,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
              <div class="form-group">
                 <label>Email</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
             </div>
              <div class="form-group">
                 <label>Phone Number</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                <input type="text" name="mobileno" class="form-control <?php echo (!empty($mobileno_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $mobileno; ?>">
+                <span class="invalid-feedback"><?php echo $mobileno_err; ?></span>
             </div>
              <div class="form-group">
                 <label>Address</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                <input type="text" name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $address; ?>">
+                <span class="invalid-feedback"><?php echo $address_err; ?></span>
             </div>
              <div class="form-group">
                 <label>Pincode</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                <input type="text" name="pincode" class="form-control <?php echo (!empty($pincode_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $pincode; ?>">
+                <span class="invalid-feedback"><?php echo $pincode_err; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
