6052100902a4e094c92fbf869f3316178d102c98
vik15 <vikram.chennuri@gmail.com>
2021-10-28 19:48:46 +0530

    aligned navbar items to right

--- prod_phones.php
+++ prod_phones.php
@@ -82,7 +82,7 @@
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form> -->
 
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
           <li class="nav-item" style="margin-right: 2%;">
             <a class="nav-link active" aria-current="page" href="index.php?page=profile">
             <span class="material-icons md-18">account_circle</span></a>
